dde79d3dd34b8ca1bbf55d28c22f950a88b40039
giana94 <giana.hardin@gmail.com>
2024-03-03 18:25:33 -0500

    CRUD Functionality

---
+++ BlockBusterWebApp/Controllers/AdminController.cs
@@ -0,0 +1,100 @@
+﻿using BlockBuster;
+using BlockBuster.Models;
+using BlockBusterWebApp.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BlockBusterWebApp.Controllers
+{
+	public class AdminController : Controller
+	{
+		// GET: AdminController
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		// GET: AdminController/Details/5
+		public ActionResult Details(int id)
+		{
+			var movie = BlockBusterBasicFunctions.GetFullMovieById(id);
+			return View(movie);
+		}
+
+		// GET: AdminController/Create
+		public ActionResult Create()
+		{
+			ViewBag.GenreId = DropDownFormatter.FormatGenres();
+			ViewBag.DirectorId = DropDownFormatter.FormatDirectors();
+			return View();
+		}
+
+		// POST: AdminController/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(Movie movieToCreate)
+		{
+			try
+			{
+                BlockBusterAdminFunctions.AddMovie(movieToCreate);
+                return RedirectToAction("Movies", "Home");
+            }
+			catch
+			{
+				return View();
+			}
+
+			
+		}
+
+		// GET: AdminController/Edit/5
+		public ActionResult Edit(int id)
+		{
+			var movie = BlockBusterBasicFunctions.GetFullMovieById(id);
+			ViewBag.GenreId = DropDownFormatter.FormatGenres();
+			ViewBag.DirectorId = DropDownFormatter.FormatDirectors();
+			return View(movie);
+
+		}
+
+		// POST: AdminController/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(Movie movieToEdit)
+		{
+			try
+			{
+				BlockBusterAdminFunctions.EditMovie(movieToEdit);
+				return RedirectToAction("Movies", "Home");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: AdminController/Delete/5
+		public ActionResult Delete(int id)
+		{
+			var movie = BlockBusterBasicFunctions.GetFullMovieById(id);
+			return View(movie);
+		}
+
+		// POST: AdminController/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(Movie movie)
+		{
+			try
+			{
+				BlockBusterAdminFunctions.DeleteMovie(movie.MovieId);
+				return RedirectToAction("Movies", "Home");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
+}
